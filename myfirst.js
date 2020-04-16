f30f7ec04fb2b7bb4898a278998e2bed530322d6
Jenifer Leitao <jenifer.leitao@gmail.com>
2020-04-16 14:20:58 -0700

    Update myfirst.js

--- myfirst.js
+++ myfirst.js
@@ -1,6 +1,7 @@
 var http = require ('http');
 
+//creates a server object:
 http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
-}).listen(8080);
+    res.writeHead(200, {'Content-Type': 'text/html'}); //write a response to the client
+    res.end('Hello World!'); //end the response
+}).listen(8080); //the server object listens on port 8080
